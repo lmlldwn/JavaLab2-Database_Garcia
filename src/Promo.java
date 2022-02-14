f33fc9d0a2ce7b6962d6a1182185d083408ed59e
Lemuel Aldwin Garcia <lemuelaldwin.garcia@gcash.com>
2022-02-14 15:03:35 +0800

    Initial commit - Lab03

---
+++ src/Promo.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Promo {
+    private String Promo_Code;
+    private String Details;
+    private String Short_Code;
+    private Date Start_Date;
+    private Date End_Date;
+
+}
---
+++ src/SMS.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class SMS {
+    private String MSISDN;
+    private String RECIPIENT;
+    private String SENDER;
+    private String SHORT_CODE;
+    private String TRANSACTION_ID;
+    private Date TIMESTAMP;
+
+
+}
