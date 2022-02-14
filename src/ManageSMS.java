ab2caf92b5c548d6d006e9a2fa00df3d1127872d
Lemuel Aldwin Garcia <lemuelaldwin.garcia@gcash.com>
2022-02-14 15:14:07 +0800

    Added interface "ManageSMS"

---
+++ src/ManageSMS.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+//lab 03.1.2 - classes
+public interface ManageSMS {
+    void insertSMS();
+    String retrieveSMSByDate(Date startDate, Date endDate);
+    String retrieveSMSByPromoCode(String promoCode);
+    String retrieveSMSByMSISDN(long msisdn);
+    String retrieveSMSSystemSent();
+    String retrieveSMSSystemReceived();
+    String retrieveSMSBroadcast(ArrayList<Long> msisdns);
+}
